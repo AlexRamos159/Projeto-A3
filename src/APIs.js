94ad5b3bd2fbb4fdca8f4d4dd3d311d230ca6e0b
Wendel Silva Davi <wendeldavi777@gmail.com>
2024-05-02 20:53:17 -0300

    Adição do prompt para gerar receitas.

--- src/APIs.js
+++ src/APIs.js
@@ -7,6 +7,9 @@ const openai = new OpenAI(apiKey);
 
 const chamarAPI = async (prompt) => {
     try {
+        const prePrompt = "Por favor, caso seja uma receita válida, prepare a seguinte receita no formato 'título, ingredientes e modo de preparo': "
+        prompt = prePrompt + prompt;
+
         const model = 'gpt-3.5-turbo';
         const role = 'user';
         const completion = await openai.chat.completions.create({
@@ -14,7 +17,6 @@ const chamarAPI = async (prompt) => {
             model: model
         });
         const resposta = completion.choices[0].message.content;
-        console.log('Resposta da API:', resposta);
         return resposta;
     } catch (error) {
         console.error('Erro ao chamar a API:', error);
