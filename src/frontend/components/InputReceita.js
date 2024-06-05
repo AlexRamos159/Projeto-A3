a1780bec4d5ef94bdb416fca19c6c8d44717b293
Alex Ramos <lex00159@gmail.com>
2024-06-05 18:06:26 -0300

    Foi adicionado um texto dentro da entrada para haver mais interação com o usuário.

--- src/frontend/components/InputReceita.js
+++ src/frontend/components/InputReceita.js
@@ -4,7 +4,7 @@ import styles from './styles/InputReceita.module.css'
 const InputReceita = (props) => {
     return (
         <div>
-            <input className={styles.inputReceita} type="text" value={props.value} onChange={props.onChange} />
+            <input className={styles.inputReceita} type="text" placeholder="Procure suas receitas..."value={props.value} onChange={props.onChange} />
         </div>
     );
 }
